1b97ffbe0d9d95edc24b567bd12bd657ef0c4402
regeldso <rvcoding@gmail.com>
2018-03-14 23:45:56 +0300

    Async Writes in Redux

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -15,6 +15,11 @@ class ManageCoursePage extends React.Component {
         this.saveCourse = this.saveCourse.bind(this);
     }
 
+    componentWillReceiveProps(nextProps){
+        if(this.props.course.id != nextProps.course.id){
+            this.setState({course: Object.assign({}, nextProps.course)});
+        }
+    }
     updateCourseState(event){
         const field = event.target.name;
         let course = this.state.course;
@@ -25,6 +30,7 @@ class ManageCoursePage extends React.Component {
     saveCourse(event) {
         event.preventDefault();
         this.props.actions.saveCourse(this.state.course);
+        this.context.router.push('/courses');
     }
 
     render() {
@@ -42,13 +48,28 @@ class ManageCoursePage extends React.Component {
         
 ManageCoursePage.propTypes = {
     course: PropTypes.object.isRequired,
-    actions: PropTypes.object.isRequired,
-    authors: PropTypes.array.isRequired
+    authors: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
 };
 
+ManageCoursePage.contextTypes = {
+    router: PropTypes.object.isRequired
+};
+
+function getCourseById(courses, id){
+    const course = courses.filter(course => course.id == id);
+    if(course) return course[0];
+    return null;
+}
+
 function mapStateToProps(state, ownProps) {
+    const courseId = ownProps.params.id;
+
     let course = {id:'', watchHref:'', title:'', authorId:'',length:'', category:''};
 
+    if (courseId && state.courses.length > 0){
+        course = getCourseById(state.courses, courseId);
+    }
     const authorsFormattedForDropdown = state.authors.map(author => {
         return {
             value: author.id,
